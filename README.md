b38f3c40fb781e9c79a4bbcd0344f507e5bdec36
Nitish Jadia <th3karkota@gmail.com>
2019-08-19 21:47:37 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# uohLogin
+# University of Hyderabad Network Login Script
+
 Shell script to automatically login on UoH network.
+Cronjob runs every minutes and the script pings `8.8.8.8` to check if you are connected to internet. If not then uses `curl` for login.
+
+## Installation
+
+1. Put `authenticate.sh` in your a **cron** dir
+
+    ```bash
+    mkdir $HOME/cron
+    vim $HOME/cron/authenticate.sh
+    ```
+
+    **NOTE:** Change the enrollment number and password to your own.
+
+2. Add cronjob for automatic login.
+
+    ```bash
+    crontab -e
+    ```
+
+    Add the following line to the cron
+
+    ```vim
+    */1 * * * * /bin/bash -c "$HOME/cron/authenticate.sh"
+    ```
+
+    That's all folks!
---
+++ authenticate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+:'
+UoH automatic network login script
+Author: Nitish Jadia
+Date: 2019-08-19
+Version: 1.0
+'
+userName="18mcmt13"
+passwd=""
+
+#read passwd < /home/nitish/.zeus/uohPasswd.txt
+
+if ! ping -4 -c 1 8.8.8.8 > /dev/null 2>&1; then
+    curl -sS 'http://192.168.56.2:8090/login.xml' -H 'Origin: http://192.168.56.2:8090' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.9,hi;q=0.8,en-IN;q=0.7' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Referer: http://192.168.56.2:8090/httpclient.html' -H 'Connection: keep-alive' -H 'DNT: 1' --data "mode=191&username=$userName&password=$passwd&a=1566218693573&producttype=0" --compressed --insecure > /dev/null 2>&1
+fi
